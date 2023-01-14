600175c63c601ce7218235ab3779eac25bd6a6b7
jacob-gerun <77089997+jacob-gerun@users.noreply.github.com>
2023-01-14 11:17:46 -0700

    Removed Default React code, added emotion

--- trip_planning/src/App.js
+++ trip_planning/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+const style = css({
+  margin: 10,
+  padding: 10,
+  backgroundColor: '#eee',
+})
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div css={style} >
+      <p> Hello World</p>
     </div>
   );
 }
