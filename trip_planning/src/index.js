8d7e9dc775a8acd13abaa12fbb1ca15f3088909f
jacob-gerun <77089997+jacob-gerun@users.noreply.github.com>
2023-01-14 11:48:51 -0700

    Started navBar

--- trip_planning/src/index.js
+++ trip_planning/src/index.js
@@ -1,13 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
 import './index.css';
 import App from './App';
+import Root from "./routes/root";
+import Contact from "./routes/contact";
 import reportWebVitals from './reportWebVitals';
+import ErrorPage from "./routes/error-page";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+  }, {
+    path: "contacts/:contactId",
+    element: <Contact />,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
